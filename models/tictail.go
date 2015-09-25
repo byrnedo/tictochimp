0633fe39c61d5e7fdcf3d7ffb04969fa4a8e7e4f
Donal Byrne <donalb@pagero.com>
2015-09-25 16:34:24 +0200

    Added GetAllOrders func

--- models/tictail.go
+++ models/tictail.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/byrnedo/tictochimp/utils"
+	"time"
 )
 
 type Tictail struct {
@@ -36,7 +37,7 @@ type MeStoreResponse struct {
 
 type MeResponse struct {
 	Email  string
-	Stores string
+	Stores []MeStoreResponse
 }
 
 func (m *Tictail) GetMe() (*MeResponse, error) {
@@ -44,6 +45,67 @@ func (m *Tictail) GetMe() (*MeResponse, error) {
 	responseData := MeResponse{}
 
 	err := m.client.Get(m.url + "/me")
+	if err != nil {
+		return nil, errors.New("Failed to do GET request: " + err.Error())
+	}
+
+	status := m.client.ResponseStatus()
+	if status != "200" && status != "201" {
+		return nil, errors.New("Got non-success status code: " + status)
+	}
+
+	err = json.Unmarshal(m.client.ResponseBody(), &responseData)
+
+	return &responseData, nil
+}
+
+type OrdersResponse struct {
+	Customer    *OrdersCustomerResponse
+	Transaction *OrdersTransactionResponse
+}
+
+type OrdersCustomerResponse struct {
+	Name       string
+	Language   string
+	Country    string
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+	ID         string
+	Email      string
+}
+type OrdersTransactionResponse struct {
+	Reference     string
+	Status        string
+	PaidAt        time.Time `json:"paid_at"`
+	PendingReason string    `json:"pending_reason"`
+	Processor     string
+}
+
+type OrderItemResponse struct {
+	Currency string
+	Price    int
+	Product  *OrderItemProductResponse
+	Quantity int
+}
+
+type OrderItemProductResponse struct {
+	Title       string
+	Status      string
+	StoreUrl    string `json:"store_url"`
+	Description string
+	StoreID     string    `json:"store_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	ModifiedAt  time.Time `json:"modified_at"`
+	Price       int
+	Currency    string
+	ID          string
+}
+
+func (m *Tictail) GetAllOrders(storeID string) (*OrdersResponse, error) {
+
+	responseData := OrdersResponse{}
+
+	err := m.client.Get(m.url + "/" + storeID + "/orders")
 	if err != nil {
 		return nil, errors.New("Failed to do GET request: " + err.Error())
 	}
--- models/tictail_test_responses.go
+++ models/tictail_test_responses.go
@@ -47,4 +47,227 @@ const (
   "appstore_currency": "USD",
   "sandbox": false
 }`
+	TICTAIL_MOCK_GET_PRODUCTS_200_RESPONSE = `[{
+    "status": "published",
+    "store_url": "http://testuser.tictail.com",
+    "description": "",
+    "store_name": "TestUser's Store",
+    "store_id": "3MpE",
+    "unlimited": true,
+    "created_at": "2015-09-16T15:07:05.000000",
+    "title": "TestProduct",
+    "modified_at": "2015-09-23T18:44:12.000000",
+    "slug": "testproduct",
+    "price": 9900,
+    "currency": "SEK",
+    "store_subdomain": "testuser",
+    "likes": 0,
+    "variations": [
+      {
+        "title": null,
+        "modified_at": "2015-09-23T09:53:53.000000",
+        "created_at": "2015-09-16T15:07:05.000000",
+        "unlimited": true,
+        "id": "Ek2Y",
+        "quantity": null
+      }
+    ],
+    "images": [
+      {
+        "original_height": 2048,
+        "sizes": {
+          "2000": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=2000",
+          "30": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=30",
+          "300": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=300",
+          "45": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=45",
+          "50": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=50",
+          "40": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=40",
+          "640": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=640",
+          "75": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=75",
+          "100": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=100",
+          "500": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=500",
+          "1000": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png?size=1000"
+        },
+        "url": "https://images.ttcdn.co/media/i/product/311676-6543a7898516436daa620e362620f643.png",
+        "created_at": "2015-09-20T12:55:45.000000",
+        "modified_at": "2015-09-20T12:55:45.000000",
+        "original_width": 2048,
+        "id": "MjzN"
+      },
+      {
+        "original_height": 2048,
+        "sizes": {
+          "2000": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=2000",
+          "30": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=30",
+          "300": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=300",
+          "45": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=45",
+          "50": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=50",
+          "40": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=40",
+          "640": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=640",
+          "75": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=75",
+          "100": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=100",
+          "500": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=500",
+          "1000": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png?size=1000"
+        },
+        "url": "https://images.ttcdn.co/media/i/product/311676-f9346272ee8545e499934345c960cf2f.png",
+        "created_at": "2015-09-20T12:55:45.000000",
+        "modified_at": "2015-09-20T12:55:45.000000",
+        "original_width": 2048,
+        "id": "MjzP"
+      },
+      {
+        "original_height": 2048,
+        "sizes": {
+          "2000": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=2000",
+          "30": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=30",
+          "300": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=300",
+          "45": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=45",
+          "50": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=50",
+          "40": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=40",
+          "640": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=640",
+          "75": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=75",
+          "100": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=100",
+          "500": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=500",
+          "1000": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png?size=1000"
+        },
+        "url": "https://images.ttcdn.co/media/i/product/311676-1eb2d98f09b5481ea35723a42e9e3245.png",
+        "created_at": "2015-09-20T12:55:45.000000",
+        "modified_at": "2015-09-20T12:55:45.000000",
+        "original_width": 2048,
+        "id": "MjzQ"
+      }
+    ],
+    "id": "mbYi",
+    "categories": [
+      {
+        "title": "TestProduct",
+        "created_at": "2015-09-20T12:55:37.000000",
+        "modified_at": null,
+        "slug": "testproduct",
+        "parent_id": null,
+        "product_count": 1,
+        "position": 0,
+        "id": "6PCE"
+      }
+    ],
+    "quantity": null
+  }
+]`
+	TICTAIL_MOCK_GET_ORDERS_200_RESPONSE = `[{
+    "customer": {
+      "name": "Test Customer",
+      "language": "en",
+      "country": "SE",
+      "created_at": "2015-09-25T10:04:38.000000",
+      "modified_at": null,
+      "id": "cLnj",
+      "email": "test.test@gmail.com"
+    },
+    "transaction": {
+      "reference": "xxx",
+      "status": "paid",
+      "paid_at": "2015-09-25T10:06:40.680296",
+      "pending_reason": null,
+      "processor": "stripe"
+    },
+    "attribution": "storefront",
+    "modified_at": "2015-09-25T10:06:44.083114",
+    "discounts": [],
+    "items": [
+      {
+        "currency": "SEK",
+        "price": 9900,
+        "product": {
+          "status": "published",
+          "store_url": null,
+          "description": ""
+          "store_name": null,
+          "store_id": "3MpE",
+          "unlimited": true,
+          "created_at": "2015-09-16T15:07:05.000000",
+          "title": "TestProduct",
+          "modified_at": "2015-09-23T18:44:12.000000",
+          "variation": null,
+          "slug": "testproduct",
+          "price": 9900,
+          "currency": "SEK",
+          "store_subdomain": null,
+          "likes": 0,
+          "images": [
+          ],
+          "id": "mbYi",
+          "categories": [
+            {
+              "title": "Product Title",
+              "created_at": "2015-09-20T12:55:37.000000",
+              "modified_at": null,
+              "slug": "product",
+              "parent_id": null,
+              "product_count": 1,
+              "position": 0,
+              "id": "6PCE"
+            }
+          ],
+          "quantity": null
+        },
+        "quantity": 1
+      }
+    ],
+    "completed": "false",
+    "invoice_fee": 0,
+    "messages": [],
+    "number": 4109746,
+    "id": "rqwk",
+    "note": null,
+    "currency": "SEK",
+    "shipping_alternative": {
+      "maximum_delivery_days": 4,
+      "regions": [
+        "SE"
+      ],
+      "excluded_regions": [],
+      "description": null,
+      "free_at_price": null,
+      "created_at": "2015-07-30T23:25:32.000000",
+      "title": "Frakt",
+      "modified_at": "2015-09-23T18:35:10.000000",
+      "requires_customer_phone": false,
+      "price": 4900,
+      "currency": "SEK",
+      "id": "3paE",
+      "minimum_delivery_days": 2
+    },
+    "fullfilment": {
+      "status": "unhandled",
+      "price": 0,
+      "provider": null,
+      "currency": "SEK",
+      "tracking_number": null,
+      "receiver": {
+        "city": "test",
+        "street_line2": null,
+        "name": "Test Customer",
+        "zip": "",
+        "country": "SE",
+        "phone": null,
+        "state": null,
+        "street": ""
+      },
+      "shipped_at": null,
+      "vat": {
+        "currency": "SEK",
+        "price": 0,
+        "rate": "0.000000"
+      }
+    },
+    "price": 9900,
+    "created_at": "2015-09-25T10:06:40.639978",
+    "vat": {
+      "currency": "SEK",
+      "price": 560,
+      "rate": "0.059957"
+    },
+    "prices_include_vat": true
+  }
+]`
 )
